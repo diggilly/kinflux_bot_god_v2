90f95550c60fbb9b2a86cbbce3bce038c0e58a8e
diggilly <126244085+diggilly@users.noreply.github.com>
2023-07-10 17:43:00 +0300

    Update _autoReply.js

--- plugins/_autoReply.js
+++ plugins/_autoReply.js
@@ -4,6 +4,10 @@ export async function all(m) {
     // when someone sends a group link to the bot's dm
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('open this link')) && !m.isBaileys && !m.isGroup) {
      this.sendButton(m.chat, `*Invite bot to a group* 
+     
+        // when someone sends a group link to the bot's dm
+    if ((m.mtype === 'hi' || m.text.startsWith('kinlflux bot') || m.text.startsWith('hello')) && !m.isBaileys && !m.isGroup) {
+     this.sendButton(m.chat, `*i am kinflux bot how can i help you reply with .list to see bots list  .owner to see bts owner .menu to see bots menu* 
         
   Hallo @${m.sender.split('@')[0]} 
   you can rent the bot to join a group or contact owner 
