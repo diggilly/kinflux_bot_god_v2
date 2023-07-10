71af84c2b5d2f51d05b517271d2c7f4e3629a53b
diggilly <126244085+diggilly@users.noreply.github.com>
2023-07-10 21:46:37 +0300

    Update gc-setdesc.js

--- plugins/gc-setdesc.js
+++ plugins/gc-setdesc.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, args }) => {
     await conn.groupUpdateDescription(m.chat, `${args.join(" ")}`);
-    m.reply('*✅Success changing The description of the group*')
+    m.reply('*🔥Success changing The description of the group*🥂')
     }
     handler.help = ['Setdesc <text>']
     handler.tags = ['group']
