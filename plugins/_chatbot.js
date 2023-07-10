648182f60a6529cc80d13e30ad7a76c9d445bb21
diggilly <126244085+diggilly@users.noreply.github.com>
2023-07-10 18:06:18 +0300

    Update _chatbot.js

--- plugins/_chatbot.js
+++ plugins/_chatbot.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
-const BRAINSHOP_BID = '176001';
-const BRAINSHOP_KEY = 'M4fzqfe99b3THOYi';
+const BRAINSHOP_BID = '176217';
+const BRAINSHOP_KEY = 'knXmUkn9zcHAk09T';
 
 export async function before(m, { conn }) {
   if (m.isBaileys && m.fromMe) {
