88617f2359b16ad4ca5d6f6f29b2f7f70ef3800d
diggilly <126244085+diggilly@users.noreply.github.com>
2023-07-11 18:02:54 +0300

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -10,13 +10,13 @@ let handler = async(m, { conn, usedPrefix, command }) => {
     
 To help us keep improving, consider following us on Instagram. Every new follower brings us joy and motivation! 
 
-Our Instagram: @asli_guru_2k23
+Our threads for Instagram: https://threads.net/@kinfluxdigital
 
 Remember, even the smallest support can make a huge difference!
 
 Additionally, if you'd like to contribute directly, you can use the attached Google Pay QR code. Just scan it with your Google Pay app to send a donation. Your support means the world to us!`;
 
-    let img = 'https://telegra.ph/file/005c11a341e1f038786e5.jpg'; 
+    let img = 'https://threads.net/@kinfluxdigital'; 
 
     console.log("Sending message and image...");
     await conn.sendFile(m.chat, img, 'donation.jpg', message, m, false, rpyp);
