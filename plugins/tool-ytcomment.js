62ae3916e6df2248a1335eada6f65c558fd2e723
diggilly <126244085+diggilly@users.noreply.github.com>
2023-07-12 16:41:35 +0300

    Update tool-ytcomment.js

--- plugins/tool-ytcomment.js
+++ plugins/tool-ytcomment.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn, text }) => {
     if (!text) throw 'No Text'
     conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/youtube-comment', {
     avatar: await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-    comment: text,
+    comment: text,  
     username: conn.getName(m.sender)
-    }), 'error.png', '*THANKS FOR COMMENT*', m)
+    }), 'error.png', '*THANKS FOR THE COMMENT FROM KINFLUX BOT*', m)
     }
     handler.help = ['ytcomment <comment>']
     handler.tags = ['maker'] 
