25bea7ef1efa5105d573c1adddfe0d3d5342c807
diggilly <126244085+diggilly@users.noreply.github.com>
2023-07-12 18:14:55 +0300

    Update maker-tweet.js

--- plugins/maker-tweet.js
+++ plugins/maker-tweet.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, text }) => {
   
     const url = `https://some-random-api.com/canvas/misc/tweet?displayname=${encodeURIComponent(displayName)}&username=${encodeURIComponent(username)}&avatar=${encodeURIComponent(avatar)}&comment=${encodeURIComponent(text)}&replies=${encodeURIComponent(replies)}&retweets=${encodeURIComponent(retweets)}&theme=${encodeURIComponent(theme)}`;
   
-    conn.sendFile(m.chat, url, 'tweet.png', '*THANKS FOR TWEETING*', m);
+    conn.sendFile(m.chat, url, 'tweet.png', '*THANKS FOR TWEETING. from kinflux bot*', m);
   };
   
   handler.help = ['tweet <comment>'];
