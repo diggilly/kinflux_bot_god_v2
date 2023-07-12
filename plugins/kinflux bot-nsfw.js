82228aec94b4240589d5f8856e8a8ea88fca82eb
diggilly <126244085+diggilly@users.noreply.github.com>
2023-07-12 18:32:15 +0300

    Update and rename Guru-nsfw.js to kinflux bot-nsfw.js

--- plugins/kinflux bot-nsfw.js
+++ plugins/kinflux bot-nsfw.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!global.db.data.chats[m.chat].nsfw) throw `🚫 group doesnt supprt nsfw \n\n enable it by \n*${usedPrefix}enable* nsfw`
   let user = global.db.data.users[m.sender].age
-  if (user < 17) throw m.reply(`❎ uneed to be atleast 18 years`)
+  if (user < 17) throw m.reply(`❎ uneed to be atleast 18 years🥵`)
 m.react(rwait)
 
 let res = await fetch(`https://fantox-apis.vercel.app/${command}`)
