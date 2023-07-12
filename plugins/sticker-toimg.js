de60525b9db6f1516dc1da3f0e72f7eec27c7738
diggilly <126244085+diggilly@users.noreply.github.com>
2023-07-12 18:02:00 +0300

    Update sticker-toimg.js

--- plugins/sticker-toimg.js
+++ plugins/sticker-toimg.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     if (!/sticker/.test(mime)) throw notStickerMessage
     let media = await q.download()
     let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-    await conn.sendFile(m.chat, out, 'out.png', '*✅ Here you have*', m)
+    await conn.sendFile(m.chat, out, 'out.png', '*✅ MADE WITH LOVE BY KINFLUX BOT. Here you have*', m)
 }
 handler.help = ['toimg <sticker>']
 handler.tags = ['sticker']
