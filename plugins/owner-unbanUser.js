0fedffce2bc16c3ebbe60da2c06a31563e3be11d
diggilly <126244085+diggilly@users.noreply.github.com>
2023-07-12 18:10:28 +0300

    Update owner-unbanUser.js

--- plugins/owner-unbanUser.js
+++ plugins/owner-unbanUser.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
     let user = global.db.data.users[who]
-    if (!who) throw `✳️ Tag or mention the user to unban`
+    if (!who) throw `✳️ Tag or mention the user to Allow him/her to use kinflux bot`
     let users = global.db.data.users
     users[who].banned = false
     conn.reply(m.chat, `
