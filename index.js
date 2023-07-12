0928887a0d8c70d9ce85ce3de29fef6683336751
diggilly <126244085+diggilly@users.noreply.github.com>
2023-07-12 19:00:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('GURU -  BOT', {
+say('kinflux - bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`'${name}' By @GURU._`, {
+say(`'${name}' By @kinfluxdigal._`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
