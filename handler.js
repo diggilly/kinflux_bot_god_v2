1d17cc5393d5521a74ff3765887a5591016201bf
kinflux digital <126244085+diggilly@users.noreply.github.com>
2023-07-14 17:07:22 +0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -778,15 +778,15 @@ TO DEACTIVE , PRESS
 
 global.dfail = (type, m, conn) => {
     let msg = {
-        rowner: '*ᴏɴʟʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ* • This command can only be used by the *Creator of the bot*',
-        owner: '*ᴏɴʟʏ ᴏᴡɴᴇʀ* • This command can only be used by the *Bot Owner',
-        mods: '*ᴏɴʟʏ ᴍᴏᴅᴇʀᴀᴛᴏʀ* •This function is only for *For Bot moderators*',
-        premium: '*ᴏɴʟʏ ᴘʀᴇᴍɪᴜᴍ* • This command is for *Premium members only',
-        group: '*ɢʀᴏᴜᴘ ᴄʜᴀᴛ* • This command can only be used in groups',
-        private: '*ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ* • This command can only be used in the *private chat of the Bot*',
-        admin: '*ᴏɴʟʏ ᴀᴅᴍɪɴ* • This command is only for *Group Admins*',
-        botAdmin: '*ᴏɴʟʏ ʙᴏᴛ ᴀᴅᴍɪɴ* • To use this command I must be *Admin!*',
-        unreg: '*ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ʀᴇɢɪsᴛᴇʀᴇᴅ ʏᴇᴛ* •  Sign in to use this feature Typing:\n\n*/reg name.age*\n\n📌Example : */reg GURU.20*', 
+        rowner: '*ᴏɴʟʏ *≡ 🎌KINFLUX 𝘽𝙊𝙏 🎌 ᴅᴇᴠᴇʟᴏᴘᴇʀ* • This command can only be used by the *Creator of the bot*',
+        owner: '*ᴏɴʟʏ *≡ 🎌KINFLUX 𝘽𝙊𝙏 🎌 ᴏᴡɴᴇʀ* • This command can only be used by the *Bot Owner',
+        mods: '*ᴏɴʟʏ *≡ 🎌KINFLUX 𝘽𝙊𝙏 🎌 ᴍᴏᴅᴇʀᴀᴛᴏʀ* •This function is only for **≡ 🎌KINFLUX 𝘽𝙊𝙏 🎌 Bot moderators*',
+        premium: '*ᴏɴʟʏ *≡ 🎌KINFLUX 𝘽𝙊𝙏 🎌 ᴘʀᴇᴍɪᴜᴍ* • This command is for **≡ 🎌KINFLUX 𝘽𝙊𝙏 🎌 Premium members only',
+        group: '*≡ 🎌KINFLUX 𝘽𝙊𝙏 🎌 *ɢʀᴏᴜᴘ ᴄʜᴀᴛ* • This command can only be used in *≡ 🎌KINFLUX 𝘽𝙊𝙏 🎌 active groups',
+        private: '* 🎌KINFLUX 𝘽𝙊𝙏 🎌 ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ* • This command can only be used in the *≡ 🎌KINFLUX 𝘽𝙊𝙏 🎌 private chat of the Bot*',
+        admin: '*ᴏɴʟʏ ≡ 🎌KINFLUX 𝘽𝙊𝙏 🎌 ᴀᴅᴍɪɴ* • This command is only for *≡ 🎌KINFLUX 𝘽𝙊𝙏 🎌Group Admins*',
+        botAdmin: '*ᴏɴʟʏ ʙᴏᴛ ᴀᴅᴍɪɴ* • To use this command *≡ 🎌KINFLUX 𝘽𝙊𝙏 🎌 must be *Admin!*',
+        unreg: '*ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ʀᴇɢɪsᴛᴇʀᴇᴅ ʏᴇᴛ* •  Sign in to use this feature Typing:\n\n*/reg name.age*\n\n📌Example : */reg kinflux.20*', 
         restrict: '*ʀᴇsᴛʀɪᴄᴛ* • This feature is *disabled*',
     }[type]
     if (msg) return m.reply(msg)
