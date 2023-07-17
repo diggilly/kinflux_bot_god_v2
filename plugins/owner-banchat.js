6d8385017d57a046f5cbe1472a9adc7f82844107
kinflux digital <126244085+diggilly@users.noreply.github.com>
2023-07-17 10:06:40 +0300

    Update owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -3,7 +3,7 @@
 let handler = async (m, { conn, isOwner, isAdmin, isROwner }) => {
     if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('✅ The bot was deactivated in this group')
+    m.reply('✅ kinflux bot was deactivated in this group')
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
