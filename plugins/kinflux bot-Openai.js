ad7cafd8981230f1b148202e581c3781fe2fec2a
kinflux digital <126244085+diggilly@users.noreply.github.com>
2023-09-03 11:18:49 +0300

    Update kinflux bot-Openai.js

--- plugins/kinflux bot-Openai.js
+++ plugins/kinflux bot-Openai.js
@@ -1,16 +1,18 @@
 import fetch from 'node-fetch';
-import { ChatGpt } from '../lib/scraper.js';
+
 let handler = async (m, { text, usedPrefix, command }) => {
-    if (!text) throw `*Enter a request or an order to use ChatGpt*\n\n*Example*\n* ${usedPrefix + command} series 2022 Netflix*\n* ${usedPrefix + command} tell us a crazy joke*`;
-    try {
-      const systemMessage = 'Analyzing your request...';
-      const response = await ChatGpt(text, systemMessage);
-      m.reply(`${response.result}`.trim());
-    } catch (error) {
-      console.error(error);
-      throw `*ERROR*`;
-    }
-  };
+  if (!text) throw `*Enter a request or an order to use ChatGpt*\n\n*Example*\n* ${usedPrefix + command} Latest Netflix seres*\n* ${usedPrefix + command} write a JS code*`;
+
+  try {
+    const response = await fetch(`https://gurugpt4-85987f3ed9b3.herokuapp.com/api/gpt4?query=${encodeURIComponent(text)}`);
+    const data = await response.json();
+    const { response: result } = data; 
+    m.reply(result.trim()); 
+  } catch (error) {
+    console.error('Error:', error); 
+    throw `*ERROR*`;
+  }
+};
 
 handler.command = ['bro', 'chatgpt', 'ai', 'siri'];
 handler.diamond = false;
