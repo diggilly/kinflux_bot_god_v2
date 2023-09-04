017d99e78f20f0596a6c1fbd83538378d680eaee
kinflux digital <126244085+diggilly@users.noreply.github.com>
2023-09-04 20:39:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['2250789747147', 'kinflux bot', true],
-  ['254110377776', 'zamzack254', true], 
+  ['254110377776', 'kinflux bot', true],
+  ['254110077535', 'zamzack254', true], 
   [''] 
 ] //Number of owners
 
