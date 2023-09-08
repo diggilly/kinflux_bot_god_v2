a1dac48a2140aae5b6053614a35cbf3e25d30475
kinflux digital <126244085+diggilly@users.noreply.github.com>
2023-09-09 00:58:59 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['254740654689', 'kinflux bot', true],
+  ['22589747147', 'kinflux bot', true],
   ['254110377776', 'zamzack254', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['254740654689','254110377776','254765777423'] 
-global.prems = ['254110077535', '254110377776', '254740654689']
+global.mods = ['22589747147','254110377776','254765777423'] 
+global.prems = ['22589747147', '254110377776', '254740654689']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
