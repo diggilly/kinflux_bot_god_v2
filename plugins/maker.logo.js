1369e973b4ef9c9d110e466747f4f94e1491d2f5
kinflux digital <126244085+diggilly@users.noreply.github.com>
2023-11-13 23:56:30 +0300

    Update maker.logo.js

--- plugins/maker.logo.js
+++ plugins/maker.logo.js
@@ -1,62 +1,110 @@
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-	 let name = conn.getName(m.sender)
-    let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.imgur.com/whjlJSf.jpg')
-    let user = global.db.data.users[m.sender]
-   let tee = `✳️ Enter a short text\n\n📌 Example  : *${usedPrefix + command}* kinflux`
-   let too = `✳️ Separate the text with a *+* \n\n📌 Example : \n*${usedPrefix + command}* kinflux *+* BOT`
-    m.react(rwait)
-let type = (command).toLowerCase()
-switch (type) {
-    case 'marvel': 
-	if (!text) throw tee
-	let di = global.API('fgmods', '/api/textpro/marvel', { text }, 'apikey')
-	conn.sendFile(m.chat, di, 'logo.png', `✅ Result`, m)
-	m.react(done)
-	break 
-     case 'ninja': 
-	if (!text) throw tee
-	let mh = global.API('fgmods', '/api/textpro/ninja', { text }, 'apikey')
-	conn.sendFile(m.chat, mh, 'logo.png', `✅ Result`, m)
-	m.react(done)
-	break 
-     case 'tiktok': 
-	if (!text) throw tee
-	let ch = global.API('fgmods', '/api/textpro/tiktok', { text }, 'apikey')
-	conn.sendFile(m.chat, ch, 'logo.png', `✅ Result`, m)
-	m.react(done)
-	break  
-     case 'lightglow': 
-	if (!text) throw tee
-	let ut = global.API('fgmods', '/api/textpro/lightglow', { text }, 'apikey')
-	conn.sendFile(m.chat, ut, 'logo.png', `✅ Result`, m)
-	m.react(done)
-	break 
-     case 'layer': 
+	let tee = `✳️ Enter a short text\n\n📌 Example  : *${usedPrefix + command}* KINFLUX`
+	let too = `✳️ Separate the text with a *+* \n\n📌 Example : \n*${usedPrefix + command}* KINFLUX *+* BOT`
+	
+	m.react(rwait);
+	
+	let type = command.toLowerCase();
+	
+	switch (type) {
+	  case 'gfx1':
+		if (!text) throw tee;
+		let chut = `https://api.caliph.biz.id/api/kaneki?nama=${encodeURIComponent(text)}&apikey=caliphkey`;
+		conn.sendFile(m.chat, chut, 'logo.png', `✅ Result`, m);
+		m.react(done);
+		break;
+		
+	  case 'gfx2': 
+		if (!text) throw too;
+		if (!text.includes('+')) throw too;
+		let [a, b] = text.split('+');
+		let loda = `https://api.caliph.biz.id/api/girlneko?nama=${encodeURIComponent(a.trim())}&nama2=${encodeURIComponent(b.trim())}&apikey=caliphkey`;
+		conn.sendFile(m.chat, loda, 'logo.png', `✅ Result`, m);
+		
+		m.react(done);
+		break;
+		
+	  case 'gfx3':
+		if (!text) throw tee;
+		let cp = `https://api.caliph.biz.id/api/rem?nama=${encodeURIComponent(text.trim())}&apikey=caliphkey`;
+		conn.sendFile(m.chat, cp, 'logo.png', `✅ Result`, m);
+		m.react(done);
+		break;
+		
+	  case 'gfx4': 
+		if (!text) throw tee;
+		let gandu = `https://api.caliph.biz.id/api/textpro/matrix?text=${encodeURIComponent(text)}&apikey=caliphkey`;
+		conn.sendFile(m.chat, gandu, 'logo.png', `✅ Result`, m);
+		m.react(done);
+		break;
+		case 'gfx5':
+    if (!text) throw tee
+    const apiUrll = `https://api.lolhuman.xyz/api/textprome/jokerlogo?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
+    conn.sendFile(m.chat, apiUrll, 'logo.png', '✅ Result', m);
+    m.react('✅');
+    break;
+
+	case 'gfx6': 
 	if (!text) throw too
 	if (!text.includes('+')) throw too  
-	let [a, b] = text.split`+`   
-	let lr = global.API('fgmods', '/api/textpro/layered', { text: a, text2: b}, 'apikey')
-	conn.sendFile(m.chat, lr, 'logo.png', `✅ Result`, m)
-	m.react(done)
-	break  
-     case 'aglow': 
-	if (!text) throw tee
-	let da = global.API('fgmods', '/api/textpro/advancedglow', { text }, 'apikey')
-	conn.sendFile(m.chat, da, 'logo.png', `✅ Result`, m)
+	let [c, d] = text.split`+`   
+	const apiUrl = `https://api.lolhuman.xyz/api/textprome2/lionlogo?apikey=${lolkeysapi}&text1=${encodeURIComponent(c)}&text2=${encodeURIComponent(d)}`
+	conn.sendFile(m.chat, apiUrl, 'logo.png', `✅ Result`, m)
 	m.react(done)
 	break 
-     case 'diamond': 
-	if (!text) throw tee
-	let de = global.API('fgmods', '/api/textpro/diamond', { text }, 'apikey')
-	conn.sendFile(m.chat, de, 'logo.png', `✅ Result`, m)
-	m.react(done)
+	case 'gfx7': 
+	if (!text) throw too;
+	if (!text.includes('+')) throw too;
+	let [e, f] = text.split('+');
+	let oda = `https://api.lolhuman.xyz/api/photooxy2/battlefield4?apikey=${lolkeysapi}&text1=${encodeURIComponent(e.trim())}&text2=${encodeURIComponent(f.trim())}`;
+	conn.sendFile(m.chat, oda, 'logo.png', `✅ Result`, m);
+	
+	m.react(done);
 	break 
-  default:
-} 
-} 
-handler.help = ['gfx5', 'marvel', 'ninja', 'tiktok', 'lightglow', 'layer', 'aglow', 'diamond']
-handler.tags = ['maker']
-handler.command = /^(gfx5|marvel|ninja|tiktok|lightglow|layer|aglow|diamond)$/i
-handler.diamond = true
+	case 'gfx8': 
+	if (!text) throw tee;
+	let rand = `https://api.lolhuman.xyz/api/ephoto1/anonymhacker?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
+	conn.sendFile(m.chat, rand, 'logo.png', `✅ Result`, m);
+	m.react(done);
+	break;
+	case 'gfx9': 
+	if (!text) throw tee;
+	let randi = `https://api.lolhuman.xyz/api/ephoto1/avatarlolnew?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
+	conn.sendFile(m.chat, randi, 'logo.png', `✅ Result`, m);
+	m.react(done);
+	break;
+	case 'gfx10': 
+	if (!text) throw tee;
+	let randu = `https://api.lolhuman.xyz/api/ephoto1/avatardota?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
+	conn.sendFile(m.chat, randu, 'logo.png', `✅ Result`, m);
+	m.react(done);
+	break;
+	case 'gfx11': 
+	if (!text) throw too;
+	if (!text.includes('+')) throw too;
+	let [g, h] = text.split('+');
+	let od = `https://api.lolhuman.xyz/api/ephoto2/codwarzone?apikey=${lolkeysapi}&text1=${encodeURIComponent(g.trim())}&text2=${encodeURIComponent(h.trim())}`;
+	conn.sendFile(m.chat, od, 'logo.png', `✅ Result`, m);
+	
+	m.react(done);
+	break 
+	case 'gfx12': 
+	if (!text) throw tee;
+	let rr = `https://api.lolhuman.xyz/api/ephoto1/freefire?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
+	conn.sendFile(m.chat, rr, 'logo.png', `✅ Result`, m);
+	m.react(done);
+	break;
 
-export default handler
+	  default:
+	} 
+  };
+  
+  handler.help = ['gfx1', 'gfx2', 'gfx3', 'gfx4', 'gfx5', 'gfx6', 'gfx7', 'gfx8', 'gfx9', 'gfx10', 'gfx11', 'gfx12'];
+  handler.tags = ['maker'];
+  handler.command = /^(gfx1|gfx2|gfx3|gfx4|gfx5|gfx6|gfx7|gfx8|gfx9|gfx10|gfx11|gfx12)$/i;
+  handler.diamond = false;
+  
+  export default handler;
+  
+
+  
